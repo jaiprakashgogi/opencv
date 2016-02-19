347e55399953bd2b885e633c6de39527b600791c
Jai Prakash <jaiprakashgogi@Jais-MBP.fios-router.home>
2016-02-19 12:02:25 -0500

    KLT Features

--- FeatureLKT/src/FeatureLKT.cpp
+++ FeatureLKT/src/FeatureLKT.cpp
@@ -5,59 +5,113 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
+#include "opencv2/video/tracking.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/videoio/videoio.hpp"
+#include "opencv2/highgui/highgui.hpp"
 
-#include "opencv2/opencv.hpp"
-//#include <stdio.h>
-//#include <stdlib.h>
-#include <vector>
+#include <iostream>
+#include <ctype.h>
 
 using namespace cv;
 using namespace std;
 
+Point2f point;
+bool addRemovePt = false;
+
 RNG rng(12345);
 
-int main(int, char**) {
-	VideoCapture cap(0); // open the default camera
-	if (!cap.isOpened())  // check if we succeeded
-		return -1;
+int main(int argc, char** argv) {
+	VideoCapture cap;
+	TermCriteria termcrit(TermCriteria::COUNT | TermCriteria::EPS, 20, 0.03);
+	Size subPixWinSize(10, 10), winSize(31, 31);
+	const int MAX_COUNT = 500;
+	bool needToInit = true;
+	vector<Scalar> color;
 
-	vector<Point> corners;
-	double qualityLevel = 0.01;
-	double minDistance = 10;
-	int blockSize = 3;
-	bool useHarrisDetector = false;
-	int maxCorners = 100;
-	double k = 0.04;
-	Mat matGray;
-	Mat matCopy;
-	for (;;) {
-		Mat frame;
-		cap >> frame; // get a new frame from camera
-		cvtColor(frame, matGray, COLOR_BGR2GRAY);
-		  goodFeaturesToTrack( matGray,
-		               corners,
-		               maxCorners,
-		               qualityLevel,
-		               minDistance,
-		               Mat(),
-		               blockSize,
-		               useHarrisDetector,
-		               k );
-		  matCopy = frame.clone();
-		//GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
-		//Canny(edges, edges, 0, 30, 3);
-		  cout<<"** Number of corners detected: "<<corners.size()<<endl;
-		  int r = 4;
-		  for( int i = 0; i < corners.size(); i++ )
-		     { circle( matCopy, corners[i], r, Scalar(rng.uniform(0,255), rng.uniform(0,255),
-		              rng.uniform(0,255)), -1, 8, 0 ); }
+	cap.open(0);
 
-		  /// Show what you got
-		imshow( "tracked", matCopy );
-		//imshow("edges", matGray);
-		if (waitKey(30) >= 0)
-			break;
+	if (!cap.isOpened()) {
+		cout << "Could not initialize capturing...\n";
+		return 0;
 	}
-	// the camera will be deinitialized automatically in VideoCapture destructor
+
+	namedWindow("LK Demo", 1);
+
+	Mat gray, prevGray, image, frame;
+	vector<Point2f> points[2];
+
+	for(int i= 0 ; i< MAX_COUNT ; i++){
+		color.push_back(Scalar(rng.uniform(0,255), rng.uniform(0,255),rng.uniform(0,255)));
+	}
+
+	for (;;) {
+		cap >> frame;
+		if (frame.empty())
+			break;
+
+		frame.copyTo(image);
+		cvtColor(image, gray, COLOR_BGR2GRAY);
+
+		if (needToInit) {
+			// automatic initialization
+			goodFeaturesToTrack(gray, points[1], MAX_COUNT, 0.01, 10, Mat(), 3,
+					0, 0.04);
+			cornerSubPix(gray, points[1], subPixWinSize, Size(-1, -1),
+					termcrit);
+			addRemovePt = false;
+		} else if (!points[0].empty()) {
+			vector<uchar> status;
+			vector<float> err;
+			if (prevGray.empty())
+				gray.copyTo(prevGray);
+			calcOpticalFlowPyrLK(prevGray, gray, points[0], points[1], status,
+					err, winSize, 3, termcrit, 0, 0.001);
+			size_t i, k;
+			for (i = k = 0; i < points[1].size(); i++) {
+				if (addRemovePt) {
+					if (norm(point - points[1][i]) <= 5) {
+						addRemovePt = false;
+						continue;
+					}
+				}
+
+				if (!status[i])
+					continue;
+
+				points[1][k++] = points[1][i];
+				circle(image, points[1][i], 3, color.at(i), -1, 8);
+			}
+			points[1].resize(k);
+		}
+
+		if (addRemovePt && points[1].size() < (size_t) MAX_COUNT) {
+			vector<Point2f> tmp;
+			tmp.push_back(point);
+			cornerSubPix(gray, tmp, winSize, Size(-1, -1), termcrit);
+			points[1].push_back(tmp[0]);
+			addRemovePt = false;
+		}
+
+		needToInit = false;
+		imshow("LK Demo", image);
+
+		char c = (char) waitKey(10);
+		if (c == 27)
+			break;
+		switch (c) {
+		case 'r':
+			needToInit = true;
+			break;
+		case 'c':
+			points[0].clear();
+			points[1].clear();
+			break;
+		}
+
+		std::swap(points[1], points[0]);
+		cv::swap(prevGray, gray);
+	}
+
 	return 0;
 }
